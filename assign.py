890ea1df86e9b531642e202a03faf7ed67617885
Leshili <leshili.bangs@binghamuni.edu.ng>
2024-11-18 07:24:20 +0100

    Add files via upload

---
+++ assign.py
@@ -0,0 +1,36 @@
+def a():
+
+    distance = float(input("Value of the distance?"))
+    time = float(input(" Value of time taken? "))
+    print(distance * time)
+
+    a();
+
+def b():
+    # speed = distance / time
+    distance = float(input("Value of the distance?"))
+    time = float(input("Value of time taken?"))
+    print(distance/time)
+
+b();
+
+def c():
+    mass = float(input("Input mass?"))
+    velocity = float(input("Input velocity"))
+    print(mass*velocity)
+
+    c();
+
+def d():
+    mass = float(input("Input mass?"))
+    acceleration = float(input("Input acceleration?"))
+    print(mass*acceleration)
+
+    d();
+
+def e():
+    power = float(input("what is the power?"))
+    time = float(input("what is the time?"))
+    print(power*time)
+
+    e();
